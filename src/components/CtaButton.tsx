9f402e636e8bfdda71763f63467e47a884ca864f
Kamil <kplewinski7@gmail.com>
2026-02-02 09:47:37 +0100

    Add price-section

--- src/components/CtaButton.tsx
+++ src/components/CtaButton.tsx
@@ -15,7 +15,8 @@ export default function CtaButton({
     `cta-btn rounded-[50px]  px-[1.5em] py-[.75em] font-semibold text-lg text-white uppercase tracking-wide xl:text-xl ${className}`,
     {
       "bg-custom-darkBlue hover:bg-custom-blue": variant === "dark",
-      "bg-custom-blue hover:bg-custom-darkBlue": variant === "light",
+      "bg-custom-blue hover:bg-custom-darkBlue shadow-[2px_3px_6px_rgba(0,102,183,0.9)]":
+        variant === "light",
     },
   );
 
---
+++ src/components/sections/price-section/PackCards.tsx
@@ -0,0 +1,152 @@
+import CtaButton from "../../CtaButton";
+
+type CardVariant = "small" | "big";
+
+interface Card {
+  cardTitle: string;
+  cardSubTitle: React.ReactNode;
+  cardPrice: string;
+  cardInfo: React.ReactNode;
+  variant: CardVariant;
+}
+
+export default function PackCards() {
+  const cards: Card[] = [
+    {
+      cardTitle: "Start",
+      cardSubTitle: (
+        <>
+          Strona One Page <br />
+          (WordPress)
+        </>
+      ),
+      cardPrice: "od 700zł",
+      cardInfo: (
+        <>
+          Ilość podstron: 1 (One Page) <br />
+          Responsywny design
+          <br />
+          Formularz kontaktowy
+          <br />
+          Google Maps
+          <br />
+          Galeria
+          <br />
+          Podstawowa optymalizacja SEO
+          <br />
+          Certyfikat SSL
+        </>
+      ),
+      variant: "small",
+    },
+    {
+      cardTitle: "Standard",
+      cardSubTitle: (
+        <>
+          Strona do 5 podstron <br />
+          (WordPress)
+        </>
+      ),
+      cardPrice: "od 1200zł",
+      cardInfo: (
+        <>
+          Ilość Podstron: do 5<br />
+          Responsywny design
+          <br />
+          Formularz kontaktowy
+          <br />
+          Google Maps
+          <br />
+          Galeria
+          <br />
+          Podstawowa optymalizacja SEO
+          <br />
+          Certyfikat SSL <br />
+          Intergracja z social mediami <br />
+          Wizytówka Google
+        </>
+      ),
+      variant: "big",
+    },
+    {
+      cardTitle: "Premium",
+      cardSubTitle: (
+        <>
+          Strona do 10 podstron lub
+          <br />
+          programowana “od zera”
+        </>
+      ),
+      cardPrice: "od 1700zł",
+      cardInfo: (
+        <>
+          WordPress lub HTML/CSS/JS <br />
+          WordPress do 10 Podstron
+          <br />
+          Responsywny design
+          <br />
+          Formularz kontaktowy
+          <br />
+          Google Maps, Galeria
+          <br />
+          Podstawowa optymalizacja SEO
+          <br />
+          Integracja z social mediami
+          <br />
+          Certyfikat SSL
+          <br />
+          Wizytówka Google
+        </>
+      ),
+      variant: "small",
+    },
+  ];
+
+  const baseClasses =
+    "relative  px-[2em] mb-[5em] text-md text-center bg-white border-4 border-custom-darkBlue rounded-lg shadow-[0_0px_10px_rgba(13,153,255,0.75)] overflow-hidden opacity-100 scale-100 translate-y-0 transition-all duration-[.75s] xl:mb-0 xl:px-[3em]";
+
+  const topBadge = (
+    <div className="pack-icon absolute left-27.5 top-6 w-full py-[.5em] bg-custom-blue -rotate-45 xl:-left-35 xl:top-8 z-[-1]">
+      <p className="text-white text-md font-bold uppercase tracking-[.5rem]">
+        Top
+      </p>
+    </div>
+  );
+  const renderedCards = cards.map((card) => {
+    const sizeClasses =
+      card.variant === "small"
+        ? "sm:h-[570px] xl:w-[380px]"
+        : "sm:h-[600px] xl:w-[400px]";
+
+    return (
+      <div key={card.cardTitle} className={`${baseClasses} ${sizeClasses}`}>
+        {card.variant === "big" && topBadge}
+        <p className="pack-title mt-[1em] text-2xl font-bold uppercase">
+          {card.cardTitle}
+        </p>
+        <p className="mt-[.5em] mb-[1.2em]">{card.cardSubTitle}</p>
+        <p className="pack-price text-2xl">
+          <span className="text-custom-darkBlue font-bold">
+            {card.cardPrice}
+          </span>{" "}
+          netto
+        </p>
+        <p className="pack-info mt-[1.2em] leading-8 text-left">
+          {card.cardInfo}
+        </p>
+        <CtaButton
+          className={`my-[1.5em] px-[.5em] sm:absolute sm:bottom-0 sm:left-[50%] sm:translate-x-[-50%] text-[1rem]! font-light! ${card.variant === "big" ? "w-62.5" : ""}`}
+          variant="light"
+        >
+          {card.variant === "big" ? "Najlepszy wybór" : "Wybierz"}
+        </CtaButton>
+      </div>
+    );
+  });
+
+  return <>{renderedCards}</>;
+}
+
+//Do animacji ma być opacity-0 zamiast opacity-100
+// scale-50 zamiast scale-100
+// translate-y-8 zamiast translate-y-0
