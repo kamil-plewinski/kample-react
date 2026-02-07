6543d710efe7954a2f9bf93ba985d66f167c0d49
Kamil <kplewinski7@gmail.com>
2026-02-07 14:59:28 +0100

    Add privacy policy page

--- src/pages/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage.tsx
@@ -1,7 +1,346 @@
 export default function PrivacyPolicyPage() {
   return (
-    <div>
-      <h1 className="bg-gray-500">Polityka Prywatności</h1>
+    <div className="bg-custom-dark text-white">
+      {" "}
+      <div className="wrapper pt-[1em]">
+        <div className="title-container mt-[7em] py-[3em]">
+          <h2 className="text-3xl font-semibold text-center">
+            Polityka prywatności
+          </h2>
+          <p className="mt-[2em] text-center">
+            Polityka prywatności opisuje zasady przetwarzania przez nas
+            informacji na Twój temat, w tym danych osobowych oraz ciasteczek,
+            czyli tzw. cookies.
+          </p>
+        </div>
+        <div className="decor-line h-px w-full mb-[2em] bg-custom-light"></div>
+        <h3 className="px-5 text-3xl font-semibold">1. Informacje ogólne</h3>
+        <p className="px-5 py-[2em]">
+          1. Niniejsza polityka dotyczy Serwisu www, funkcjonującego pod adresem
+          url: kample.pl <br />
+          2. Operatorem serwisu oraz Administratorem danych osobowych jest:
+          Kamil Plewiński Wilków 12, 58-125, Pszenno <br />
+          3. Adres kontaktowy poczty elektronicznej operatora:
+          kplewinski7@gmail.com <br />
+          4. Operator jest Administratorem Twoich danych osobowych w odniesieniu
+          do danych podanych dobrowolnie w Serwisie. <br />
+          5. Serwis wykorzystuje dane osobowe w następujących celach: <br />
+          a. Prezentacja oferty lub informacji <br />
+          6. Serwis realizuje funkcje pozyskiwania informacji o użytkownikach i
+          ich zachowaniu w następujący sposób: <br />
+          a. Poprzez dobrowolnie wprowadzone w formularzach dane, które zostają
+          wprowadzone do systemów Operatora. <br />
+          b. Poprzez zapisywanie w urządzeniach końcowych plików cookie (tzw.
+          „ciasteczka”).
+        </p>
+        <h3 className="px-5 text-3xl font-semibold">
+          2. Wybrane metody ochrony danych stosowane przez Operatora
+        </h3>
+        <p className="px-5 py-[2em]">
+          1. Miejsca logowania i wprowadzania danych osobowych są chronione w
+          warstwie transmisji (certyfikat SSL). Dzięki temu dane osobowe i dane
+          logowania, wprowadzone na stronie, zostają zaszyfrowane w komputerze
+          użytkownika i mogą być odczytane jedynie na docelowym serwerze.
+          <br />
+          2. Istotnym elementem ochrony danych jest regularna aktualizacja
+          wszelkiego oprogramowania, wykorzystywanego przez Operatora do
+          przetwarzania danych osobowych, co w szczególności oznacza regularne
+          aktualizacje komponentów programistycznych. <br />
+        </p>
+        <h3 className="px-5 text-3xl font-semibold">3. Hosting</h3>
+        <p className="px-5 py-[2em]">
+          1. Serwis jest hostowany (technicznie utrzymywany) na serwerach
+          operatora: lh.pl
+          <br />
+          2. Firma hostingowa w celu zapewnienia niezawodności technicznej
+          prowadzi logi na poziomie serwera. Zapisowi mogą podlegać:
+          <br />
+          a. zasoby określone identyfikatorem URL (adresy żądanych zasobów -
+          stron, plików),
+          <br />
+          b. czas nadejścia zapytania,
+          <br />
+          c. czas wysłania odpowiedzi,
+          <br />
+          d. nazwę stacji klienta - identyfikacja realizowana przez protokół
+          HTTP,
+          <br />
+          e. informacje o błędach jakie nastąpiły przy realizacji transakcji
+          HTTP,
+          <br />
+          f.adres URL strony poprzednio odwiedzanej przez użytkownika (referer
+          link) - w przypadku gdy przejście do Serwisu nastąpiło przez odnośnik,
+          <br />
+          g. informacje o przeglądarce użytkownika,
+          <br />
+          h. informacje o adresie IP,
+          <br />
+          i. informacje diagnostyczne związane z procesem samodzielnego
+          zamawiania usług poprzez rejestratory na stronie,
+          <br />
+          j. informacje związane z obsługą poczty elektronicznej kierowanej do
+          Operatora oraz wysyłanej przez Operatora.
+          <br />
+        </p>
+        <h3 className="px-5 text-3xl font-semibold">
+          4. Twoje prawa i dodatkowe informacje o sposobie wykorzystania danych
+        </h3>
+        <p className="px-5 py-[2em]">
+          1. niektórych sytuacjach Administrator ma prawo przekazywać Twoje dane
+          osobowe innym odbiorcom, jeśli będzie to niezbędne do wykonania
+          zawartej z Tobą umowy lub do zrealizowania obowiązków ciążących na
+          Administratorze. Dotyczy to takich grup odbiorców: <br />
+          a. osoby upoważnione przez nas, pracownicy i współpracownicy, którzy
+          muszą mieć dostęp do danych osobowych w celu wykonywania swoich
+          obowiązków, <br />
+          b. firma hostingowa,
+          <br />
+          c. firmy obsługująca mailingi,
+          <br />
+          d. firmy obsługująca komunikaty SMS,
+          <br />
+          e. firmy, z którymi Administrator współpracuje w zakresie marketingu
+          własnego,
+          <br />
+          f. kurierzy,
+          <br />
+          g. ubezpieczyciele,
+          <br />
+          h. kancelarie prawne i windykatorzy,
+          <br />
+          i. banki,
+          <br />
+          j. operatorzy płatności,
+          <br />
+          k. organy publiczne.
+          <br />
+          2. Twoje dane osobowe przetwarzane przez Administratora nie dłużej,
+          niż jest to konieczne do wykonania związanych z nimi czynności
+          określonych osobnymi przepisami (np. o prowadzeniu rachunkowości). W
+          odniesieniu do danych marketingowych dane nie będą przetwarzane dłużej
+          niż przez 3 lata. <br />
+          3. Przysługuje Ci prawo żądania od Administratora: <br />
+          a.dostępu do danych osobowych Ciebie dotyczących, <br />
+          b. ich sprostowania, <br />
+          c.usunięcia,
+          <br />
+          d. ograniczenia przetwarzania, <br />
+          e. oraz przenoszenia danych. <br />
+          4. Przysługuje Ci prawo do złożenia sprzeciwu w zakresie przetwarzania
+          wskazanego w pkt 3.2 wobec przetwarzania danych osobowych w celu
+          wykonania prawnie uzasadnionych interesów realizowanych przez
+          Administratora, w tym profilowania, przy czym prawo sprzeciwu nie
+          będzie mogło być wykonane w przypadku istnienia ważnych prawnie
+          uzasadnionych podstaw do przetwarzania, nadrzędnych wobec Ciebie
+          interesów, praw i wolności, w szczególności ustalenia, dochodzenia lub
+          obrony roszczeń.
+          <br />
+          5. Na działania Administratora przysługuje skarga do Prezesa Urzędu
+          Ochrony Danych Osobowych, ul. Stawki 2, 00-193 Warszawa. <br />
+          6. Podanie danych osobowych jest dobrowolne, lecz niezbędne do obsługi
+          Serwisu. <br />
+          7. W stosunku do Ciebie mogą być podejmowane czynności polegające na
+          zautomatyzowanym podejmowaniu decyzji, w tym profilowaniu w celu
+          świadczenia usług w ramach zawartej umowy oraz w celu prowadzenia
+          przez Administratora marketingu bezpośredniego. <br />
+          8. Dane osobowe nie są przekazywane od krajów trzecich w rozumieniu
+          przepisów o ochronie danych osobowych. Oznacza to, że nie przesyłamy
+          ich poza teren Unii Europejskiej.
+        </p>
+        <h3 className="px-5 text-3xl font-semibold">
+          5. Informacje w formularzach
+        </h3>
+        <p className="px-5 py-[2em]">
+          1. Serwis zbiera informacje podane dobrowolnie przez użytkownika, w
+          tym dane osobowe, o ile zostaną one podane. <br />
+          2. Serwis może zapisać informacje o parametrach połączenia (oznaczenie
+          czasu, adres IP). <br />
+          3. Serwis, w niektórych wypadkach, może zapisać informację ułatwiającą
+          powiązanie danych w formularzu z adresem e-mail użytkownika
+          wypełniającego formularz. W takim wypadku adres e-mail użytkownika
+          pojawia się wewnątrz adresu url strony zawierającej formularz. <br />
+          4. Dane podane w formularzu są przetwarzane w celu wynikającym z
+          funkcji konkretnego formularza, np. w celu dokonania procesu obsługi
+          zgłoszenia serwisowego lub kontaktu handlowego, rejestracji usług itp.
+          Każdorazowo kontekst i opis formularza w czytelny sposób informuje, do
+          czego on służy.
+        </p>
+        <h3 className="px-5 text-3xl font-semibold">6. Logi Administratora</h3>
+        <p className="px-5 py-[2em]">
+          1. Informacje zachowaniu użytkowników w serwisie mogą podlegać
+          logowaniu. Dane te są wykorzystywane w celu administrowania serwisem.
+        </p>
+        <h3 className="px-5 text-3xl font-semibold">
+          7. Istotne techniki marketingowe
+        </h3>
+        <p className="px-5 py-[2em]">
+          1. Operator stosuje analizę statystyczną ruchu na stronie, poprzez
+          Google Analytics (Google Inc. z siedzibą w USA). Operator nie
+          przekazuje do operatora tej usługi danych osobowych, a jedynie
+          zanonimizowane informacje. Usługa bazuje na wykorzystaniu ciasteczek w
+          urządzeniu końcowym użytkownika. W zakresie informacji o preferencjach
+          użytkownika gromadzonych przez sieć reklamową Google użytkownik może
+          przeglądać i edytować informacje wynikające z plików cookies przy
+          pomocy narzędzia:
+          <a
+            href="https://www.google.com/ads/preferences/"
+            className="underline"
+          >
+            Link
+          </a>
+          <br />
+          2. Operator stosuje rozwiązanie badające zachowanie użytkowników
+          poprzez tworzenie map ciepła oraz nagrywanie zachowania na stronie. Te
+          informacje są anonimizowane zanim zostaną przesłane do operatora
+          usługi tak, że nie wie on jakiej osoby fizycznej one dotyczą. W
+          szczególności nagrywaniu nie podlegają wpisywane hasła oraz inne dane
+          osobowe.
+        </p>
+        <h3 className="px-5 text-3xl font-semibold">
+          8. Informacja o plikach cookies
+        </h3>
+        <p className="px-5 py-[2em]">
+          1. Serwis korzysta z plików cookies. <br />
+          2. Pliki cookies (tzw. „ciasteczka”) stanowią dane informatyczne, w
+          szczególności pliki tekstowe, które przechowywane są w urządzeniu
+          końcowym Użytkownika Serwisu i przeznaczone są do korzystania ze stron
+          internetowych Serwisu. Cookies zazwyczaj zawierają nazwę strony
+          internetowej, z której pochodzą, czas przechowywania ich na urządzeniu
+          końcowym oraz unikalny numer. <br />
+          3. Podmiotem zamieszczającym na urządzeniu końcowym Użytkownika
+          Serwisu pliki cookies oraz uzyskującym do nich dostęp jest operator
+          Serwisu.
+          <br />
+          4. Pliki cookies wykorzystywane są w następujących celach: <br />
+          a. utrzymanie sesji użytkownika Serwisu (po zalogowaniu), dzięki
+          której użytkownik nie musi na każdej podstronie Serwisu ponownie
+          wpisywać loginu i hasła; <br />
+          b. realizacji celów określonych powyżej w części "Istotne techniki
+          marketingowe"; <br />
+          5. W ramach Serwisu stosowane są dwa zasadnicze rodzaje plików
+          cookies: „sesyjne” (session cookies) oraz „stałe” (persistent
+          cookies). Cookies „sesyjne” są plikami tymczasowymi, które
+          przechowywane są w urządzeniu końcowym Użytkownika do czasu
+          wylogowania, opuszczenia strony internetowej lub wyłączenia
+          oprogramowania (przeglądarki internetowej). „Stałe” pliki cookies
+          przechowywane są w urządzeniu końcowym Użytkownika przez czas
+          określony w parametrach plików cookies lub do czasu ich usunięcia
+          przez Użytkownika. <br />
+          6. Oprogramowanie do przeglądania stron internetowych (przeglądarka
+          internetowa) zazwyczaj domyślnie dopuszcza przechowywanie plików
+          cookies w urządzeniu końcowym Użytkownika. Użytkownicy Serwisu mogą
+          dokonać zmiany ustawień w tym zakresie. Przeglądarka internetowa
+          umożliwia usunięcie plików cookies. Możliwe jest także automatyczne
+          blokowanie plików cookies Szczegółowe informacje na ten temat zawiera
+          pomoc lub dokumentacja przeglądarki internetowej. <br />
+          7. Ograniczenia stosowania plików cookies mogą wpłynąć na niektóre
+          funkcjonalności dostępne na stronach internetowych Serwisu. <br />
+          8. Pliki cookies zamieszczane w urządzeniu końcowym Użytkownika
+          Serwisu wykorzystywane mogą być również przez współpracujące z
+          operatorem Serwisu podmioty, w szczególności dotyczy to firm: Google
+          (Google Inc. z siedzibą w USA), Facebook (Facebook Inc. z siedzibą w
+          USA), Twitter (Twitter Inc. z siedzibą w USA).
+        </p>
+        <h3 className="px-5 text-3xl font-semibold">
+          9. Zarządzanie plikami cookies - jak w praktyce wyrażać i cofać zgodę?
+        </h3>
+        <p className="px-5 py-[2em]">
+          1. Jeśli użytkownik nie chce otrzymywać plików cookies, może zmienić
+          ustawienia przeglądarki. Zastrzegamy, że wyłączenie obsługi plików
+          cookies niezbędnych dla procesów uwierzytelniania, bezpieczeństwa,
+          utrzymania preferencji użytkownika może utrudnić, a w skrajnych
+          przypadkach może uniemożliwić korzystanie ze stron www <br />
+          2. Sposób wyłączenia obsługi plików cookies zależy od przeglądarki, z
+          której Użytkownik korzysta. Poniżej wskazany został sposób, w jaki
+          Użytkownik może wyłączyć obsługę plików cookies w przeglądarce Google
+          Chrome, Firefox, Safari, Opera oraz Internet Explorer. <br />
+          a. Wyłączenie obsługi plików cookies w przeglądarce Google Chrome
+          <br />
+          1. W prawy górnym rogu przy pasku adresu przeglądarki Google Chrome
+          wybierz trzy kropki pionowe. <br />
+          2. W sekcji Prywatność bezpieczeństwo kliknij przycisk Treść. <br />
+          3. W sekcji Pliki cookies i dane stron możesz zmienić następujące
+          ustawienia plików cookies: <br />
+          - Blokuj wszystkie pliki cookies <br />
+          - Blokuj pliki cookies innych firm <br />
+          Więcej informacji o ustawieniach przeglądarki Google Chrome: <br />
+          <a
+            aria-label="Sprawdź szczegóły usawień przeglądarki"
+            href="https://support.google.com/chrome/#topic=7438008"
+            className="underline"
+          >
+            Sprawdź szczegóły usawień przeglądarki Google Chrome
+          </a>
+          b. Wyłączenie obsługi plików cookies w przeglądarce Firefox <br />
+          1. W prawym górnym rogu okna przeglądarki Firefox kliknij Otwórz menu
+          (trzy poziome kreski) i wybierz Ustawienia. <br />
+          2. Następnie z sekcji Prywatność i bezpieczeństwo wybierz zakładkę
+          ciasteczka i dane witryn. <br />
+          3. Wybierz odpowiedni poziom bezpieczeństwa lub skonfiguruj własny
+          wybierając które elementy i skrypty blokować - ciasteczka: <br />
+          - Wszystkie z zewnętrznych witryn <br />
+          - Wszystkie <br />
+          Więcej informacji o ustawieniach przeglądarki Firefox: <br />
+          <a
+            aria-label="Sprawdź szczegóły usawień przeglądarki"
+            href="https://support.mozilla.org/pl/kb/W%C5%82%C4%85czanie%20i%20wy%C5%82%C4%85czanie%20obs%C5%82ugi%20ciasteczek"
+            className="underline"
+          >
+            Sprawdź szczegóły usawień przeglądarki Firefox
+          </a>
+          c. Wyłączenie obsługi plików cookies w przeglądarce Safari <br />
+          1. Wybierz polecenie menu Safari {">"} Preferencje, kliknij w
+          Prywatność, a następnie wykonaj dowolną z poniższych czynności: <br />
+          -Blokuj wszystkie cookie <br />
+          - Zapobiegaj śledzeniu poza witryną <br />
+          Więcej informacji o ustawieniach przeglądarki Safari: <br />
+          <a
+            aria-label="Sprawdź szczegóły usawień przeglądarki"
+            href="https://support.apple.com/kb/PH19214?viewlocale=pl_PL&locale=pl_PL"
+            className="underline"
+          >
+            Sprawdź szczegóły usawień przeglądarki Safari
+          </a>
+          d. Wyłączenie obsługi plików cookies w przeglądarce Opera <br />
+          1. W prawym górnym rogu przy pasku adresu przeglądarki Opera wybierz
+          trzy linie poziome. <br />
+          2. Kliknij przejdź do pełnych ustawień przeglądarki. <br />
+          3. W sekcji Prywatność bezpieczeństwo kliknij przycisk Ciasteczka i
+          inne dane witryn. <br />
+          4. Możesz zmienić następujące ustawienia plików cookies: <br />
+          - Blokuj wszystkie pliki cookies <br />
+          - Blokuj pliki cookies innych firm <br />
+          Więcej informacji o ustawieniach przeglądarki Opera: <br />
+          <a
+            aria-label="Sprawdź szczegóły usawień przeglądarki"
+            href="http://help.opera.com/Windows/12.10/pl/cookies.html"
+            className="underline"
+          >
+            Sprawdź szczegóły usawień przeglądarki Opera
+          </a>
+        </p>
+        <p className="px-5">
+          {" "}
+          e.Wyłączenie obsługi plików cookies w przeglądarce Edge <br />
+          1.W prawy górnym rogu przy pasku adresu przeglądarki Edge wybierz trzy
+          kropki poziome. <br />
+          2. Kliknij Ustawienia. <br />
+          3. W sekcji Pliki cookie i uprawnienia witryn kliknij Zarządzaj
+          plikami cookie i danymi witryn oraz usuwaj je. <br />
+          4. Możesz zmienić następujące ustawienia plików cookies: <br />
+          - Blokuj <br />
+          - Blokuj pliki cookies innych firm <br />
+          Więcej informacji o ustawieniach przeglądarki Edge: <br />
+          <a
+            aria-label="Sprawdź szczegóły usawień przeglądarki"
+            href="https://support.microsoft.com/pl-pl/microsoft-edge"
+            className="underline"
+          >
+            Sprawdź szczegóły usawień przeglądarki Edge
+          </a>
+        </p>
+        <div className="decor-line h-px w-full mt-[2em] bg-custom-light"></div>
+      </div>
     </div>
   );
 }
