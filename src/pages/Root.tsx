c32ea386e85882da7e8762718ad346c618d6c6ec
Kamil <kplewinski7@gmail.com>
2026-02-10 14:55:15 +0100

    fix: add scroll restoration for route navigation

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,3 +1,4 @@
+import { ScrollRestoration } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -5,6 +6,7 @@ import Footer from "../components/Footer";
 export default function Root() {
   return (
     <>
+      <ScrollRestoration />
       <Header />
       <main>
         <Outlet />
